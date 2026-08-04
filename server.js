32371dc8bd53c32b2ff1652f5b2abc625fdf8282
Syeda Fizza <117969468+SFizzaR@users.noreply.github.com>
2026-08-04 23:42:34 +0500

    Refactor server.js to include new API endpoints
    Updated server configuration and added new API routes for verifying pins and crossword words.

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require("express");
-const path = require("path");
+
+const dotenv = require("dotenv").config();
+const cors = require('cors');
+
+const { verifyPin } = require('./controller/pinController');
+const { verifyCrosswordWord } = require('./controller/crosswordController');
+
+
+const supabaseAdmin = require('./config/supabaseAdmin');
 
 const app = express();
-const PORT = process.env.PORT || 5500;
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+const port = process.env.PORT || 5000;
 
-// Serve index.html for the root route
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Frontend running at http://localhost:${PORT}`);
-});
+app.post('/api/rooms/:roomNumber/verify-pin', verifyPin);
+app.post('/api/crossword/verify-word/:wordIndex', verifyCrosswordWord);
+app.use('/api/participants', require('./routes/userRoutes'));
+
+
+
+app.listen(port, ()=>{
+    console.log(`server running on port:  ${port}`)
+})
